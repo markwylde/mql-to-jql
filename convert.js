bce33417020d20a41292f9a49ca025cf1786deda
Mark Wylde <me@markwylde.com>
2020-11-07 19:18:41 +1000

    Add ne and eq

--- convert.js
+++ convert.js
@@ -32,10 +32,19 @@ function parseQuery (query) {
       return;
     }
 
-    fields.push('/[[* = :?] = :?]');
-    values.push(key);
-
-    values.push(query[key]);
+    if (query[key].$eq) {
+      fields.push('/[[* = :?] = :?]');
+      values.push(key);
+      values.push(query[key].$ne);
+    } else if (query[key].$ne) {
+      fields.push('/[[* = :?] != :?]');
+      values.push(key);
+      values.push(query[key].$ne);
+    } else {
+      fields.push('/[[* = :?] = :?]');
+      values.push(key);
+      values.push(query[key]);
+    }
   });
 
   return {
